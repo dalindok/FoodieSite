33a5244d0f09afbc9fbb88c119362b148e326607
Thonida1045 <ithonida@gmail.com>
2024-12-25 20:30:46 +0700

    change locatation of Edit

--- src/pages/UserAccount.tsx
+++ src/pages/UserAccount.tsx
@@ -3,7 +3,7 @@ import SignUp from "../components/SignUp";
 import Phone from "../components/phone/Phone";
 import Verification from "../components/phone/Verification";
 import SideAccount from "../components/SideAccount";
-import Edit from "../components/edit/Edit";
+import Edit from "../components/Edit";
 import StatusTap from "../components/userAccount/StatusTap";
 import HistoryList from "../components/userAccount/HistoryList";
 
